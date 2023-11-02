7dd27c7c144de9a4e5012707d5a0247aa1c5e4f2
Cameron Berg <54913067+camberg23@users.noreply.github.com>
2023-11-02 19:04:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -70,10 +70,14 @@ Be sure to also include all of the following links somewhere in the body of the 
 -Holland Code: https://www.truity.com/test/holland-code-career-test 
 -DISC: https://www.truity.com/test/disc-personality-test 
 
-FORMAT THE BLOG IN MARKDOWN!
+FINAL CRITICAL NOTES:
 
-THE ENTIRE BLOG SHOULD BE APPROXIMATELY 1500 WORDS. THIS IS A CRITICAL REQUIREMENT.
+1. PLEASE FORMAT THE BLOG IN MARKDOWN!
+
+2. THE ENTIRE BLOG SHOULD BE APPROXIMATELY 1500 WORDS. THIS IS A CRITICAL REQUIREMENT.
 AVOID MAKING THE BLOG TOO SHORT! IT MUST BE 1500 WORDS! YOU WILL NOT RUN OUT OF TOKENS AT THIS LENGTH!
+
+3. Do not end any section header with a colon (":"). They should be natural sounding headers appropriate to that section.
 """
 API = st.secrets['API']
 def generate_blog(profession):
